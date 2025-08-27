23986b6bf3146c98aabee78c05cda52f2a8b1e8d
vinnyp215 <vincentgpeters@gmail.com>
2025-08-27 12:09:43 +0100

    Moved main dynamics function to the top of the code for clarity

--- dynamics.py
+++ dynamics.py
@@ -5,6 +5,40 @@ import numpy as np
 import scipy as sp
 import constants 
 
+def dynamics(t, state, spacecraft):
+    """
+    Computes the time derivative of the state vector for the CubeSat.
+    
+    Args:
+        state (np.array): Current state vector [x, y, z, vx, vy, vz, q0, q1, q2, q3, wx, wy, wz].
+        
+    Returns:
+        state_derivative (np.array): Time derivative of the state vector.
+    """
+    # Unpack state vector
+    r = state[0:3]    # Position [x, y, z]
+    v = state[3:6]    # Velocity [vx, vy, vz]
+    q = state[6:10]   # Attitude quaternion [q0, q1, q2, q3]
+    w = state[10:13]  # Angular velocity [wx, wy, wz]
+    
+    # Translational dynamics (position and velocity)
+    drdt = v
+    dvdt = calculate_a_g(r) # Gravitational acceleration
+        
+    # Calculate quaternion derivative
+    dqdt = quaternion_derivative(q, w)
+    
+    # Assume no external torques for now (can be expanded later)
+    dwdt = np.zeros(3)
+    
+    # Pack derivatives into a single state derivative vector
+    state_derivative = np.zeros(13)
+    state_derivative[0:3] = drdt    # dr/dt = v
+    state_derivative[3:6] = dvdt    # dv/dt = a
+    state_derivative[6:10] = dqdt   # dq/dt
+    state_derivative[10:13] = dwdt  # dω/dt
+    
+    return state_derivative
 
 def calculate_a_g(r):
     """
@@ -46,39 +80,4 @@ def quaternion_derivative(q, w):
     ])
     
     return dqdt
-
-def dynamics(t, state, spacecraft):
-    """
-    Computes the time derivative of the state vector for the CubeSat.
-    
-    Args:
-        state (np.array): Current state vector [x, y, z, vx, vy, vz, q0, q1, q2, q3, wx, wy, wz].
-        
-    Returns:
-        state_derivative (np.array): Time derivative of the state vector.
-    """
-    # Unpack state vector
-    r = state[0:3]    # Position [x, y, z]
-    v = state[3:6]    # Velocity [vx, vy, vz]
-    q = state[6:10]   # Attitude quaternion [q0, q1, q2, q3]
-    w = state[10:13]  # Angular velocity [wx, wy, wz]
-    
-    # Translational dynamics (position and velocity)
-    drdt = v
-    dvdt = calculate_a_g(r)
-        
-    # Calculate quaternion derivative
-    dqdt = quaternion_derivative(q, w)
-    
-    # Assume no external torques for now (can be expanded later)
-    dwdt = np.zeros(3)
-    
-    # Pack derivatives into a single state derivative vector
-    state_derivative = np.zeros(13)
-    state_derivative[0:3] = drdt    # dr/dt = v
-    state_derivative[3:6] = dvdt    # dv/dt = a
-    state_derivative[6:10] = dqdt   # dq/dt
-    state_derivative[10:13] = dwdt  # dω/dt
-    
-    return state_derivative
 
